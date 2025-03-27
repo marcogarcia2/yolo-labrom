ec8423c901ac563f7f8a81f31b0585e1a5cea9a2
Marco Garcia <mmgaspargarcia@gmail.com>
2025-03-27 14:19:20 -0300

    Adding requirements

--- train.py
+++ train.py
@@ -2,21 +2,25 @@ from ultralytics import YOLO
 import torch
 import os
 
-# Hyperparameters
-EPOCHS = 50
-BATCH_SIZE = 8
-IMG_SIZE = 640
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+def main():
+    # Hyperparameters
+    EPOCHS = 50
+    BATCH_SIZE = 8
+    IMG_SIZE = 640
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-# Downloading our model
-model = YOLO("yolov8n-seg.pt")
+    # Downloading our model
+    model = YOLO("yolo11n-seg.pt")
 
-# Using GPU if it is available
-model.to(device)
-print(f"The model is at {device}.")
+    # Using GPU if it is available
+    model.to(device)
+    print(f"The model is at {device}.")
 
-# Training model on dataset
-model.train(data="dataset/data.yaml", epochs=EPOCHS, batch=BATCH_SIZE, imgsz=IMG_SIZE)
+    # Training model on dataset
+    model.train(data="dataset/data.yaml", epochs=EPOCHS, batch=BATCH_SIZE, imgsz=IMG_SIZE)
 
-# Salvar o modelo treinado
-model.save("models/yolov8n-seg_trained_0.pt")
+    # Salvar o modelo treinado
+    model.save("models/yolo11n-seg_trained_0.pt")
+
+if __name__ == "__main__":
+    main()
