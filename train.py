2afa297e73a42a2d147c06f94d2b503fab47ba8a
Marco Garcia <mmgaspargarcia@gmail.com>
2025-03-27 14:48:48 -0300

    Downloading models

--- train.py
+++ train.py
@@ -7,17 +7,18 @@ def main():
     EPOCHS = 50
     BATCH_SIZE = 8
     IMG_SIZE = 640
+    PATIENCE = 10
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
     # Downloading our model
-    model = YOLO("yolo11n-seg.pt")
+    model = YOLO("yolo11s-seg.pt")
 
     # Using GPU if it is available
     model.to(device)
     print(f"The model is at {device}.")
 
     # Training model on dataset
-    model.train(data="dataset/data.yaml", epochs=EPOCHS, batch=BATCH_SIZE, imgsz=IMG_SIZE)
+    model.train(data="dataset/data.yaml", epochs=EPOCHS, batch=BATCH_SIZE, imgsz=IMG_SIZE, patience=PATIENCE)
 
     # Salvar o modelo treinado
     model.save("models/yolo11n-seg_trained_0.pt")
