9770148ebdfef09548ccfb7ad00cf2f9435218a2
marcogarcia2 <mmgaspargarcia@gmail.com>
2025-07-29 20:41:40 -0300

    Fine tune done.

--- fine_tune.py
+++ fine_tune.py
@@ -1,51 +1,56 @@
 from ultralytics import YOLO
 from pathlib import Path
 import torch
+import colorful as cf
 
-def fine_tune_model(model_name: str, device: str):
+# A função de fine-tuning agora recebe o caminho para a pasta de resultados do treino anterior
+def fine_tune_model(previous_training_path: str, model_char: str, device: str):
+
+    model_path = Path(previous_training_path) / "weights/best.pt"
     
-    # Loading the model from models folder
-    model_path = Path("models") / model_name
+    if not model_path.exists():
+        print(f"ERROR: Could not find model at: {model_path}")
+        return
+
     model = YOLO(model_path)
 
-    # Sending it to GPU if it is avaliable
-    model.to(device)
-    print(f"The model is at {device}.")
-    
-    # Folder where the results will be saved
-    save_folder = "runs/fine_tune_"  + model_name[6] # char 's; or 'n'
+    # Ensuring that the loaded model is the one that was trained previously
+    print(cf.bold_yellow(f"Modelo carregado de: {model.ckpt_path}"))
 
-    # Tuning model based on these hyperparameters
+    # Sending model to GPU
+    model.to(device)
+    print(f"The model is at " +  cf.bold_green(f"{device}") + ".")
     
+    save_folder = "runs/fine_tune_" + model_char
+
+    # Fine Tuning training
     model.train(
-        data="fine_tuning/data.yaml",   
-        epochs=300,                 # max epochs
-        patience=50,                # early stopping: if it doesn't get better in 10 epochs
-        batch=16,                   # batch size
-        imgsz=640,                  # image size 
-        lr0=0.001,                  # initial learning rate
+        data="fine_tuning/data.yaml", 
+        epochs=300,                 
+        patience=50,
+        batch=16,
+        imgsz=640,
+        lr0=0.0001,                 # Smaller initial learning rate for fine tuning
         project=save_folder,        
         name="exp",                 
     )
     
-    # Saving our tuned model in models folder
-    model = YOLO(save_folder + "/exp/weights/best.pt")  # loads the best
-    save_name = "models/" + model_name[:-11] + "-fine-tuned.pt"
-    model.save(save_name)
+    # Saves the best model
+    best_tuned_model_path = Path(save_folder) / "exp/weights/best.pt"
+    save_name = f"models/yolo11{model_char}-seg-fine-tuned.pt"
+    final_model = YOLO(best_tuned_model_path)
+    final_model.save(save_name)
 
 
 def main():
 
-    # Checking GPU availability
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    
-    # Using custom function to fine tune model N on the available device
-    fine_tune_model('yolo11n-seg-trained.pt', device)
-    
-    # Same for model S
-    fine_tune_model('yolo11s-seg-trained.pt', device)
 
+    # Fine-tuning for model N
+    fine_tune_model("runs/train_n/exp", 'n', device)
     
+    # Fine-tuning for model S
+    fine_tune_model("runs/train_s/exp", 's', device)
 
 if __name__ == "__main__":
     main()
